66713907686689b4e97e3843f6a7807f4cd802ab
Shilpita <shilpitaroy87@gmail.com>
2016-11-11 01:11:18 -0800

    new coed

--- src/ArrayQuestions/FindFixedPoint.java
+++ src/ArrayQuestions/FindFixedPoint.java
@@ -1,7 +1,26 @@
 package ArrayQuestions;
+/**************************************************************************************
+ * Author : Shilpita Roy
+ * Date   : Nov11,2016
+ * Problem: Given an array of n distinct integers sorted in ascending order, write a function that returns a Fixed Point in the array,
+ * 		    if there is any Fixed Point present in array, else returns -1. Fixed Point in an array is an index i such that arr[i]= i. 
+ * 			Ex: Input: {-10, -5, 0, 3, 7}
+ *				Output: 3
+ *          GeekForGeeks
+ *          
+ * The time complexity for the following method is O(N).
+ * ***************************************************************************************/
+
 
 public class FindFixedPoint {
 
+	//Generic print function
+	private static void printArray(int[] arr1){
+		System.out.println();
+		for(int i: arr1)
+			System.out.print(i +"\t");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
